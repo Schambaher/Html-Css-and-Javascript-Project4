c069fe695a1cd01a5280987e4ca206c8b56af69c
Sebastian Schambaher <sebastianschambaher@gmail.com>
2021-11-08 21:56:06 -0500

    mejoramient de la logica de programación con javascript

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-  iniciarApp();
-});
 
-const numbers = document.querySelectorAll(".number");
+  const contadores = document.querySelectorAll(".contador");
+  const velocidad = 1000;
 
-function iniciarApp() {
-  numeroAcendente(numbers[0]);
-}
+  const animarContadores = () => {
+    for (const contador of contadores) {
+      const actualizarContador = () => {
+        let cantidadMaxima = parseInt(contador.dataset.cantidadTotal);
+        let valorActual = parseInt(contador.innerText);
+        let incremento = cantidadMaxima / velocidad;
 
-function numeroAcendente(elemento) {
-  for (let i = 0; i < elemento.textContent; i++) {
-    setTimeout(() => {
-      console.log(i);
-    }, 2000);
+        if(valorActual < cantidadMaxima) {
+          contador.innerText = Math.ceil(valorActual + incremento);
+          setTimeout(actualizarContador, 2);
+        }else {
+          contador.innerText = cantidadMaxima;
+        }
+      }
+      actualizarContador();
+    }
   }
-}
+  
+  // IntersectionObserver
+
+  const opciones = {
+    threshold: 0.2 // va de 0-1
+  }
+  const mostrarContadores = elementos => {
+    elementos.forEach(elemento => {
+      if (elemento.isIntersecting) {
+        setTimeout(animarContadores, 300);
+      }
+    });
+  }
+  const  observer = new IntersectionObserver(mostrarContadores, opciones);
+  contadores.forEach(contador => {
+    observer.observe(contador);
+  })
+});
